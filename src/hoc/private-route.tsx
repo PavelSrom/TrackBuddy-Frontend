92566685215396fd83d57bc0a18d0ba96061c911
Pavel Srom <srompavel98@gmail.com>
2020-12-18 12:14:06 +0100

    refactor(navigation): top navigation instead of bottom

--- src/hoc/private-route.tsx
+++ src/hoc/private-route.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route, Navigate, useLocation } from 'react-router-dom'
 import { Container } from '@material-ui/core'
 import { useAuth } from '../contexts/auth'
-import { BottomNavigation } from '../components/bottom-navigation'
+import { TopNavigation } from '../components/top-navigation'
 
 type Props = {
   path: string
@@ -19,15 +19,15 @@ export const PrivateRoute: React.FC<Props> = ({ element, ...rest }) => {
       element={
         isAuthenticated ? (
           <>
+            {location.pathname !== '/actions' && <TopNavigation />}
+
             <Container
               maxWidth="xs"
               // pb-14 because of our bottom nav height
-              className="bg-gray-100 w-screen min-h-screen absolute top-0 right-0 left-0 pb-14"
+              className="bg-gray-100 w-screen min-h-screen absolute top-0 right-0 left-0 pt-12"
             >
               {element}
             </Container>
-
-            {location.pathname !== '/actions' && <BottomNavigation />}
           </>
         ) : (
           <Navigate to="/login" />
--- src/types/navigation-item.d.ts
+++ src/types/navigation-item.d.ts
@@ -3,5 +3,5 @@ export type NavigationLink = '/dashboard' | '/journals' | '/planner' | '/habits'
 export type NavigationItem = {
   value: NavigationLink
   label: string
-  icon: React.ComponentType
+  icon: React.ElementType
 }
